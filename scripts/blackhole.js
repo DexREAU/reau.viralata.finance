b714add034b24c918937f144b86f0de75fee2d29
André Ciappina <91566252+DexREAU@users.noreply.github.com>
2023-04-01 23:47:34 -0300

    up

--- scripts/blackhole.js
+++ scripts/blackhole.js
@@ -7,6 +7,8 @@ const blackholeblur = document.querySelector('.blackhole .blur')
 
 let isInsideBlackHole = false;
 
+const statusCtnr = document.querySelector('.status_ctnr')
+
 function toggleBlackhole(e) {
     if(isInsideBlackHole == false) {
         e.preventDefault()
@@ -14,16 +16,22 @@ function toggleBlackhole(e) {
 
         blackholeborder.style.animation = 'unset'
         blackholeblur.style.animation = 'unset'
-        blackholeZoom.style.transform = 'scale(5)'
+        blackholeZoom.style.transform = 'scale(5.5)'
+
+        setTimeout(() => {
+            statusCtnr.style.top = '24vh'
+        }, 900);
     } else {
         e.preventDefault()
         isInsideBlackHole = false;
         blackholeZoom.style.transform = 'scale(1)'
 
+        statusCtnr.style.top = 'calc(50% - (clamp(6rem, 10vw, 10rem) / 2))'
+
         setTimeout(() => {
             blackholeborder.style.animation = 'movegradient 3s linear infinite'
             blackholeblur.style.animation = 'movegradient 3s linear infinite'
-        }, 600);
+        }, 1500);
     }
 }
 
